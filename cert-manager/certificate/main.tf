d3e1f1fc5245a2cf74daf159718219ca8f4024fb
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-06-11 16:04:48 -0400

    wip

---
+++ cert-manager/certificate/main.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "vault_init_identity" {
+  name       = (var.helm_name != "" ? var.helm_name : "pod-id-${var.identity_name}")
+  chart      = "${path.module}/chart"
+
+  set {
+    name  = "azureIdentity.name"
+    value = var.identity_name
+  }
+
+  set {
+    name  = "azureIdentity.resourceID"
+    value = var.identity_resource_id
+  }
+
+  set {
+    name  = "azureIdentity.clientID"
+    value = var.identity_client_id
+  }
+
+  set {
+    name  = "azureIdentityBinding.name"
+    value = "${var.identity_name}-binding"
+  }
+
+  set {
+    name  = "azureIdentityBinding.selector"
+    value = var.identity_name
+  }
+
+}
