6ae6824b93113523c49ee3d4f658bf0985af1621
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-06-17 15:45:42 -0400

    add secret_path output

--- cert-manager/certificate/output.tf
+++ cert-manager/certificate/output.tf
@@ -1,19 +1,23 @@
 output "helm_release_name" {
-    value = (var.helm_release_name != "" ? var.helm_release_name : "le-cert-${var.certificate_name}")
+  value = (var.helm_release_name != "" ? var.helm_release_name : "le-cert-${var.certificate_name}")
 }
 
 output "certificate_name" {
-    value = var.certificate_name
+  value = var.certificate_name
 }
 
 output "namespace" {
-    value = var.namespace
+  value = var.namespace
 }
 
 output "secret_name" {
-    value = var.secret_name
+  value = var.secret_name
+}
+
+output "secret_path" {
+  value = "${var.namespace}/${var.secret_path}"
 }
 
 output "issuer_ref_name" {
-    value = var.issuer_ref_name
+  value = var.issuer_ref_name
 }
