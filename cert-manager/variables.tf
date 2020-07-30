7ab40aa58b15a2068bf99af17cc1fc623f2c5f3d
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-07-30 13:06:53 -0400

    cleanup

--- cert-manager/variables.tf
+++ cert-manager/variables.tf
@@ -61,7 +61,10 @@ variable "install_crds" {
 
 variable "domains" {
   description = "map of domains to domain ids and resource groups which certificates will be generated for"
-  type        = map(object({id = string, resource_group = string}))
+  type        = map(object({
+                  id             = string # id of dns zone 
+                  resource_group = string # name of resource group containing the dns zone
+                }))
   default     = {}
 }
 
