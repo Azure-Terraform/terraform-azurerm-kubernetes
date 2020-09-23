2570ea92f097843925c0142f5f8a00ed5bfc6d6d
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-09-23 13:00:20 -0400

    WIP

--- data_sources.tf
+++ data_sources.tf
@@ -3,8 +3,9 @@ data "azuread_service_principal" "aks" {
   application_id = var.service_principal_id
 }
 
-data "azurerm_public_ip" "outbound_ips" {
-  for_each = toset(azurerm_kubernetes_cluster.aks.network_profile[0].load_balancer_profile[0].effective_outbound_ips)
-  name                = reverse(split("/", each.key))[0]
-  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
-}
+#data "azurerm_public_ip" "outbound_ips" {
+#  depends_on = [azurerm_kubernetes_cluster.aks]
+#  for_each = azurerm_kubernetes_cluster.aks.network_profile[0].load_balancer_profile[0].effective_outbound_ips
+#  name                = reverse(split("/", each.key))[0]
+#  resource_group_name = azurerm_kubernetes_cluster.aks.node_resource_group
+#}
