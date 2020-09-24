2065b6a4e36cf5e682f4682635f2cff8b7cadeb4
Tim Miller <dutsmiller@gmail.com>
2020-09-24 15:04:52 -0400

    fix service principal data object (#15)
    Co-authored-by: Tim Miller <tim.miller@risk.lexisnexis.com>

--- data_sources.tf
+++ data_sources.tf
@@ -1,4 +1,4 @@
 data "azuread_service_principal" "aks" {
-  count        = (var.use_service_principal ? 1 : 0)
-  display_name = var.service_principal_name
+  count          = (var.use_service_principal ? 1 : 0)
+  application_id = var.service_principal_id
 }
