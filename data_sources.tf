b439cadfc69feeefd45a4150c751ab9129aa7f0d
Tim Miller <tim.miller@risk.lexisnexis.com>
2020-10-15 00:38:32 -0400

    fix typo

--- data_sources.tf
+++ data_sources.tf
@@ -2,3 +2,7 @@ data "azuread_service_principal" "aks" {
   count          = (var.use_service_principal ? 1 : 0)
   application_id = var.service_principal_id
 }
+
+locals {
+  validate_windows_profile_admin_password = (var.enable_windows_node_pools ? (var.windows_profile_admin_password == "" ? file("ERROR: windows_profile_admin_password cannot be empty") : null) : null)
+}
