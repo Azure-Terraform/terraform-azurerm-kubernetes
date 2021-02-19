9ba63a5d74a827bfb1d80617a1637509795818b8
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-02-19 16:50:46 -0500

    remove uai data source

--- data_sources.tf
+++ data_sources.tf
@@ -3,12 +3,6 @@ data "azuread_service_principal" "aks" {
   application_id = var.service_principal_id
 }
 
-data "azurerm_user_assigned_identity" "aks" {
-  count               = (var.identity_type == "UserAssigned" ? (var.user_assigned_identity == null ? 0 : 1) : 0)
-  name                = var.user_assigned_identity.name
-  resource_group_name = var.user_assigned_identity.resource_group_name
-}
-
 locals {
   validate_windows_profile_admin_password = (var.enable_windows_node_pools ? (var.windows_profile_admin_password == "" ? file("ERROR: windows_profile_admin_password cannot be empty") : null) : null)
 }
