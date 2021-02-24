6d5bfa30e6c412ceb160fa748c8b83d2b2eefe92
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-02-24 15:59:12 -0500

    rewrite node_pool inputs

--- variables.tf
+++ variables.tf
@@ -73,52 +73,64 @@ variable "network_plugin" {
   default     = "kubenet"
 }
 
-node_pool_defaults = {
+variable "node_pools" {
+  description = "node pools"
+  type        = any
+  default     = { default = { name = "default" } }
+}
+
+variable "node_pool_defaults"  {
   description = "node pool defaults"
   type        = object({
-                  name                         = string
-                  vm_size                      = string
-                  availability_zones           = list(number)
-                  node_count                   = number
-                  enable_auto_scaling          = bool
-                  min_count                    = number
-                  max_count                    = number
-                  enable_host_encryption       = bool
-                  enable_node_public_ip        = bool
-                  max_pods                     = number
-                  node_labels                  = map(string)
-                  only_critical_addons_enabled = bool
-                  orchestrator_version         = string
-                  os_disk_size_gb              = number
-                  os_disk_type                 = string
-                  type                         = string
-                  tags                         = map(string)
-                  vnet_subnet_id               = string
+                  name                               = string
+                  vm_size                            = string
+                  availability_zones                 = list(number)
+                  node_count                         = number
+                  enable_auto_scaling                = bool
+                  min_count                          = number
+                  max_count                          = number
+                  enable_host_encryption             = bool
+                  enable_node_public_ip              = bool
+                  max_pods                           = number
+                  node_labels                        = map(string)
+                  only_critical_addons_enabled       = bool
+                  orchestrator_version               = string
+                  os_disk_size_gb                    = number
+                  os_disk_type                       = string
+                  type                               = string
+                  tags                               = map(string)
+                  subnet_id                          = string
+                  subnet_resource_group_name         = string
+                  subnet_network_security_group_name = string
+                  subnet_custom_route_table_id       = string
 
-                  eviction_policy              = string
-                  os_type                      = string
-                  priority                     = string
-                  proximity_placement_group_id = string
-                  spot_max_price               = string 
+                  eviction_policy                    = string
+                  os_type                            = string
+                  priority                           = string
+                  proximity_placement_group_id       = string
+                  spot_max_price                     = string 
   })
-  default     = { name                         = "default"
-                  vm_size                      = "Standard_B2s"
-                  availability_zones           = [1,2,3]
-                  node_count                   = 1
-                  enable_auto_scaling          = false
-                  min_count                    = 1
-                  max_count                    = 2
-                  enable_host_encryption       = true
-                  enable_node_public_ip        = false
-                  max_pods                     = 30
-                  node_labels                  = {}
-                  only_critical_addons_enabled = false
-                  orchestrator_version         = null
-                  os_disk_size_gb              = 128
-                  os_disk_type                 = "Managed"
-                  type                         = "VirtualMachineScaleSets"
-                  tags                         = {}
-                  vnet_subnet_id               = null
+  default     = { name                               = null
+                  vm_size                            = "Standard_B2s"
+                  availability_zones                 = [1,2,3]
+                  node_count                         = 1
+                  enable_auto_scaling                = false
+                  min_count                          = null
+                  max_count                          = null
+                  enable_host_encryption             = true
+                  enable_node_public_ip              = false
+                  max_pods                           = 30
+                  node_labels                        = {}
+                  only_critical_addons_enabled       = false
+                  orchestrator_version               = null
+                  os_disk_size_gb                    = 128
+                  os_disk_type                       = "Managed"
+                  type                               = "VirtualMachineScaleSets"
+                  tags                               = {}
+                  subnet_id                          = null
+                  subnet_resource_group_name         = null
+                  subnet_network_security_group_name = null
+                  subnet_custom_route_table_id       = null
 
                   eviction_policy              = null
                   os_type                      = "Linux"
@@ -128,44 +140,10 @@ node_pool_defaults = {
   }
 }
 
-variable "default_node_pool_node_max_count" {
-  description = "enable default node pool auto scaling (only valid with auto scaling)"
-  type        = number
-  default     = 5
-}
-
-variable "default_node_pool_availability_zones" {
-  description = "default node pool availability zones"
-  type        = list(number)
-  default     = [1,2,3]
-}
-
-variable "aks_managed_vnet" {
-  description = "use AKS managed vnet/subnet (false requires default_node_pool_subnet and node_pool_subnets is specified)"
-  type        = bool
-  default     = true
-}
-
-variable "default_node_pool_subnet" {
-  description = "name of key from node_pool_subnets map to use for default node pool"
+variable "default_node_pool" {
+  description = "Default node pool.  Value refers to key within node_pools variable."
   type        = string
-  default     = ""
-}
-
-variable "node_pool_subnets" {
-  description = "Node pool subnet info."
-  type        = map(object({
-                  id                          = string
-                  resource_group_name         = string
-                  network_security_group_name = string
-                }))
-  default     = {}
-}
-
-variable "custom_route_table_ids" {
-  description = "Custom route tables used by node pool subnets."
-  type        = map(string)
-  default     = {}
+  default     = "default"
 }
 
 variable "configure_network_role" {
