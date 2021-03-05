3df65583460a84cd7dd66cf85dc8d273f345ba2c
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-03-04 23:58:14 -0500

    add addtional validation checks

--- locals.tf
+++ locals.tf
@@ -2,18 +2,14 @@ locals {
   node_pools            = zipmap(keys(var.node_pools), [ for node_pool in values(var.node_pools) : merge(var.node_pool_defaults, node_pool) ])
   additional_node_pools = { for k,v in local.node_pools : k => v if k != var.default_node_pool}
 
-  #distinct_subnet_ids = compact(distinct([ for node_pool in local.node_pools : node_pool.subnet_id ]))
-  #match_subnets       = { for subnet in local.distinct_subnet_ids : subnet => [ for k,v in local.node_pools : k if v.subnet_id == subnet] }
-  #subnet_info         = { for k,v in local.match_subnets : 
-  #                        k => { id                          = local.node_pools[v[0]].subnet_id
-  #                               resource_group_name         = local.node_pools[v[0]].subnet_resource_group_name
-  #                               network_security_group_name = local.node_pools[v[0]].subnet_network_security_group_name
-  #                               custom_route_table_id       = local.node_pools[v[0]].subnet_custom_route_table_id }
-  #}
-  #custom_route_table_ids = toset(compact([ for subnet in local.subnet_info : subnet.custom_route_table_id ]))
+  valid_route_table_ids = (var.identity_type == "SystemAssigned" && length(var.custom_route_table_ids) > 0 ?
+                           file("ERROR: custom route tables unavailable with SystemAssigned identity type") : null)
 
-  valid_node_pools = (((local.node_pools[var.default_node_pool].type != "VirtualMachineScaleSets") && (length(local.additional_node_pools) > 0)) ?
-                       file("ERROR: multiple node pools only allowed when default node pool type is VirtualMachineScaleSets") : null)
+  validate_multiple_node_pools = (((local.node_pools[var.default_node_pool].type != "VirtualMachineScaleSets") && (length(local.additional_node_pools) > 0)) ?
+                                    file("ERROR: multiple node pools only allowed when default node pool type is VirtualMachineScaleSets") : null)
+
+  validate_default_node_pool = (local.node_pools[var.default_node_pool].os_type != "Linux" ?
+                                file("ERROR: default node pool type must be Linux") : null)
 
   validate_windows_profile_admin_password = (var.enable_windows_node_pools ? (var.windows_profile_admin_password == "" ?
                                               file("ERROR: windows_profile_admin_password cannot be empty") : null) : null)
