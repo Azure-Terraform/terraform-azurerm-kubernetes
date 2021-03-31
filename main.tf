aa04bd8a09e740366f33669186608d35f9127c9e
Joshua Hiscocks <joshua.hiscocks@lexisnexisrisk.com>
2021-03-31 18:43:50 +0100

    Fixed additional references to uai id

--- main.tf
+++ main.tf
@@ -11,8 +11,7 @@ resource "azurerm_role_assignment" "route_table_network_contributor" {
 
   scope                = each.value
   role_definition_name = "Network Contributor"
-  principal_id         = (var.user_assigned_identity == null ? azurerm_user_assigned_identity.aks.0.principal_id :
-                          var.user_assigned_identity.principal_id)
+  principal_id         = local.aks_identity_id
 }
 
 module "subnet_config" {
