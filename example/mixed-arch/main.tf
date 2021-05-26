b5da4bd7e0609f963ab7c47893917c107a61407c
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-05-26 15:30:05 -0400

    update providers in example

--- example/mixed-arch/main.tf
+++ example/mixed-arch/main.tf
@@ -2,22 +2,22 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.51.0"
+      version = "~>2.51.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">=2.3.0"
+      version = "~>2.3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.0.2"
+      version = "~>2.0.2"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">=2.0.3"
+      version = "~>2.0.3"
     }
   }
-   required_version = "~> 0.15.0"
+   required_version = "~>0.15.0"
 }
 
 provider "azurerm" {
