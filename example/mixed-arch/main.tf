f4c5cb88ff6725f1e5e6121de7157fed7ba15ca1
Tim Miller <tim.miller@risk.lexisnexis.com>
2021-05-26 15:42:50 -0400

    update versions

--- example/mixed-arch/main.tf
+++ example/mixed-arch/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>2.51.0"
+      version = "~>2.57.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -14,7 +14,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~>2.0.3"
+      version = "~>3.0.0"
     }
   }
    required_version = "~>0.15.0"
